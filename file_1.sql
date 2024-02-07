8c395c220e61bafd3408567eb2db042eca48dc92
Mihirkumar <155640442+number1DataScientist@users.noreply.github.com>
2024-02-08 00:01:09 +0530

    add file via desktop

---
+++ file_1.sql
@@ -0,0 +1,15 @@
+create database github;
+
+use github;
+
+show tables;
+
+CREATE TABLE Student_details(
+student_id VARCHAR(15),
+student_FirstName VARCHAR(20),
+student_LastName VARCHAR(20),
+student_contactNumber VARCHAR(10),
+student_age INT)
+
+
+
